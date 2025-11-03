4099b6f37cf8bcfbecaf453563c62953dd6e2100
bastgood3166666 <sansan197166666@gmail.com>
2025-11-03 19:36:00 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -101,12 +101,12 @@ const CHARS: &[char] = &[
 ];
 
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["38.91.118.78"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["38.46.15.162"];
 pub const RS_PUB_KEY: &str = "39iCJusN1Mh+H7OqXB8i10yeRlfG2ytJbtBN5y3CQhM=";
-pub const RENDEZVOUS_PORT: i32 = 12486;
-pub const RELAY_PORT: i32 = 12487;
-pub const WS_RENDEZVOUS_PORT: i32 =12488;
-pub const WS_RELAY_PORT: i32 = 12489;
+pub const RENDEZVOUS_PORT: i32 = 12586;
+pub const RELAY_PORT: i32 = 12587;
+pub const WS_RENDEZVOUS_PORT: i32 =12588;
+pub const WS_RELAY_PORT: i32 = 12589;
 
 macro_rules! serde_field_string {
     ($default_func:ident, $de_func:ident, $default_expr:expr) => {
